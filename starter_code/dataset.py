480a75a526b0d7788e351058a61adec8383c539e
anthonychen000 <anthoc@umich.edu>
2024-03-10 15:24:52 -0400

    added transforms

--- starter_code/dataset.py
+++ starter_code/dataset.py
@@ -10,6 +10,7 @@ import pandas as pd
 import torch
 import numpy.typing as npt
 import torchvision
+from torchvision import transforms
 import imageio.v3 as iio
 
 
@@ -25,9 +26,18 @@ def preprocess(image: npt.ArrayLike) -> torch.Tensor:
     """
     # Convert image to tensor
     tensor = torch.tensor(image, dtype=torch.float32)
-
+    custom_transforms = transforms.Compose([
+        transforms.RandomHorizontalFlip(p=1),  # horizontal flip
+        transforms.RandomRotation(degrees=(-20, 20)),  # rotation range of -20 to 20 degrees
+        transforms.RandomAffine(degrees=0, shear=(-20, 20)),  # shear range of -20 to 20 degrees
+        transforms.RandomAffine(degrees=0, scale=(0.8, 1.2)),  # zoom range of 0.8 to 1.2
+        transforms.RandomAffine(degrees=0, translate=(0.2, 0.2)),  # shift range of 20% of image size
+    ])
+       
+    # Add other transformations as needed
+    transformed_tensor = custom_transforms(tensor)
     # TODO: Edit this function to more preprocessing steps to improve model performance.
-    return tensor
+    return transformed_tensor
 
 
 class RvFDataset(torch.utils.data.Dataset):
