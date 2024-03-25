a56ee079b96e1b43a81cdf24d8906ff1819858c9
anthonychen000 <124098228+anthonychen000@users.noreply.github.com>
2024-03-25 00:34:06 -0400

    Update train.py

---
+++ submission/train.py
@@ -0,0 +1,18 @@
+from starter_code.dataset import get_loaders
+from starter_code.train import train_model, plot_performance, load_model
+from dataset import preprocess
+from model import Model
+train_loader, val_loader = get_loaders(preprocessor=preprocess)
+import torch
+
+model = Model()
+learning_rate = 6e-4
+weight_decay = 0.01
+optimizer = torch.optim.AdamW(params= model.parameters(), lr=learning_rate, weight_decay=weight_decay) # TODO: Change the optimizer to explore different options
+criterion = torch.nn.BCEWithLogitsLoss() # TODO: Change the criterion to explore different options
+
+history = train_model(model, criterion, optimizer, train_loader, val_loader, epochs=10)
+plot_performance(history)
+
+# Load the model from the training run
+load_model(model, "checkpoints", 0) # change epoch from 0 to something else
