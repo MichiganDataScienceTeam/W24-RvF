50cc5bdbfaf5b1e352bb098f1a80b6e419056754
anthonychen000 <124098228+anthonychen000@users.noreply.github.com>
2024-03-30 11:46:25 -0400

    Update train.py

--- submission/train.py
+++ submission/train.py
@@ -6,13 +6,35 @@ train_loader, val_loader = get_loaders(preprocessor=preprocess)
 import torch
 
 model = Model()
-learning_rate = 0.0001
-weight_decay = 0.000001
-optimizer = torch.optim.AdamW(params= model.parameters(), lr=learning_rate, weight_decay=weight_decay) # TODO: Change the optimizer to explore different options
-criterion = torch.nn.CrossEntropyLoss() # TODO: Change the criterion to explore different options
+
+# train the classifier layer first with higher learning rate
+for name, param in model.named_parameters():
+    if '_fc' not in name:
+        param.requires_grad = False
+
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
+criterion = torch.nn.CrossEntropyLoss()
+
+num_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+total_params = sum(p.numel() for p in model.parameters())
 
 history = train_model(model, criterion, optimizer, train_loader, val_loader, epochs=30)
 plot_performance(history)
 
-# Load the model from the training run
-load_model(model, "checkpoints", 0) # change epoch from 0 to something else
+# Unfreeze some feature layers
+
+for name, param in model.named_parameters():
+    if '_blocks.15' in name and 'bn' not in name:
+        param.requires_grad = True
+      
+model.source_model._conv_head.weight.requires_grad = True
+    
+num_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+total_params = sum(p.numel() for p in model.parameters())
+
+weight_decay = 1e-6
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=weight_decay)
+criterion = torch.nn.CrossEntropyLoss()
+
+history = train_model(model, criterion, optimizer, train_loader, val_loader, epochs=10)
+plot_performance(history)
