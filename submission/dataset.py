a4fbfe53212e6b2a478e3793a03d8dcd7243a0e8
anthonychen000 <124098228+anthonychen000@users.noreply.github.com>
2024-03-30 20:36:37 -0400

    Update dataset.py

--- submission/dataset.py
+++ submission/dataset.py
@@ -43,8 +43,7 @@ def preprocess2(image: npt.ArrayLike) -> torch.Tensor:
         torch.Tensor: The preprocessed image as a tensor.
     """
     # Convert image to tensor
-    tensor = torch.tensor(image, dtype=torch.float32)
-     mean = [0.485, 0.456, 0.406]  # Mean values for the RGB channels
+    mean = [0.485, 0.456, 0.406]  # Mean values for the RGB channels
     std = [0.229, 0.224, 0.225]    # Standard deviation values for the RGB channels
     tensor = torch.tensor(image, dtype=torch.float32)
     custom_transforms = transforms.Compose([
@@ -54,10 +53,9 @@ def preprocess2(image: npt.ArrayLike) -> torch.Tensor:
         transforms.RandomAffine(degrees=0, scale=(0.8, 1.2)),  # zoom range of 0.8 to 1.2
         transforms.RandomAffine(degrees=0, translate=(0.2, 0.2)),  # shift range of 20% of image size
         transforms.Lambda(lambda x: x.permute(2, 0, 1)),  # Permute to (3, 224, 224)
-        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
-        transforms.RandomGrayscale(p=0.3)
         transforms.Resize((224, 224)),  # Resize image to (224, 224)
-        transforms.Normalize(mean=mean, std=std), 
+        transforms.Normalize(mean=mean, std=std),
+        transforms.RandomGrayscale(p=0.3)
     ])
     # Add other transformations as needed
     transformed_tensor = custom_transforms(tensor)
