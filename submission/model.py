0a5bcdb523f923a0692b0e6ea86efb9f7efecd70
anthonychen000 <124098228+anthonychen000@users.noreply.github.com>
2024-03-31 15:07:36 -0400

    Update model.py

--- submission/model.py
+++ submission/model.py
@@ -1,20 +1,18 @@
-import torch
-import torchvision.models as models
 import torch.nn as nn
 from efficientnet_pytorch import EfficientNet
 
-class Model(torch.nn.Module):
+class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
 
-        source_model = EfficientNet.from_pretrained('efficientnet-b0')
+        source_model = EfficientNet.from_pretrained('efficientnet-b3')
 
         source_model._fc = nn.Sequential(
-            nn.Linear(1280, 512),
-            nn.ReLU(),
+            nn.Linear(1536, 512),
+            nn.SiLU(),
             nn.Dropout(0.5),
             nn.Linear(512, 256),
-            nn.SiLU()
+            nn.SiLU(),
             nn.Dropout(0.5),
             nn.Linear(256, 2)
         )
@@ -24,3 +22,4 @@ class Model(torch.nn.Module):
     def forward(self, x):
         x = self.source_model(x)
         return x
+
