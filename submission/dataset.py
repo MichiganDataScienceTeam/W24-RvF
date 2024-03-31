19f3dad638604118a76234ff25a7079ec8bce393
anthonychen000 <124098228+anthonychen000@users.noreply.github.com>
2024-03-31 15:08:31 -0400

    Update dataset.py

--- submission/dataset.py
+++ submission/dataset.py
@@ -13,24 +13,20 @@ def preprocess(image: npt.ArrayLike) -> torch.Tensor:
         torch.Tensor: The preprocessed image as a tensor.
     """
     # Convert image to tensor
-    tensor = torch.tensor(image, dtype=torch.float32)
-     mean = [0.485, 0.456, 0.406]  # Mean values for the RGB channels
+    mean = [0.485, 0.456, 0.406]  # Mean values for the RGB channels
     std = [0.229, 0.224, 0.225]    # Standard deviation values for the RGB channels
     tensor = torch.tensor(image, dtype=torch.float32)
     custom_transforms = transforms.Compose([
         transforms.RandomHorizontalFlip(p=0.5),  # horizontal flip
         transforms.RandomRotation(degrees=0.2),
         transforms.Lambda(lambda x: x.permute(2, 0, 1)),  # Permute to (3, 224, 224)
-        transforms.Resize((224, 224)),  # Resize image to (224, 224)
-        transforms.Normalize(mean=mean, std=std), 
+        transforms.Resize((300, 300)),  # Resize image to (224, 224)
+        transforms.Normalize(mean=mean, std=std)
     ])
+
     # Add other transformations as needed
     transformed_tensor = custom_transforms(tensor)
     return transformed_tensor
-
-import torch
-from torchvision import transforms
-import numpy.typing as npt
 
 def preprocess2(image: npt.ArrayLike) -> torch.Tensor:
     """
@@ -53,28 +49,11 @@ def preprocess2(image: npt.ArrayLike) -> torch.Tensor:
         transforms.RandomAffine(degrees=0, scale=(0.8, 1.2)),  # zoom range of 0.8 to 1.2
         transforms.RandomAffine(degrees=0, translate=(0.2, 0.2)),  # shift range of 20% of image size
         transforms.Lambda(lambda x: x.permute(2, 0, 1)),  # Permute to (3, 224, 224)
-        transforms.Resize((224, 224)),  # Resize image to (224, 224)
+        transforms.Resize((300, 300)),  # Resize image to (224, 224)
         transforms.Normalize(mean=mean, std=std),
-        transforms.RandomGrayscale(p=0.3)
+        transforms.RandomGrayscale(p=0.9)
     ])
+
     # Add other transformations as needed
     transformed_tensor = custom_transforms(tensor)
     return transformed_tensor
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
